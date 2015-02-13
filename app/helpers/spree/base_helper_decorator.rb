0a9c2650622750b432a0be9a4fbf7541c5576b26
Kent Yeung <kent.yeung2@gmail.com>
2015-02-13 08:26:54 -0500

    fixes for product
    product fixes

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,16 +1,12 @@
 Spree::BaseHelper.module_eval do
   def link_to_cart(text = nil)
-    return "" if current_spree_page?(spree.cart_path)
-
-    text = text ? h(text) : Spree.t('cart')
-    css_class = nil
 
     if current_order.nil? or current_order.item_count.zero?
-      text = "#{text}: (#{Spree.t('empty')})".html_safe
-      css_class = 'empty'
+      text = "(#{Spree.t('empty')})".html_safe
+      css_class = 'empty cartcolor'
     else
-      text = "#{text}: (#{current_order.item_count})  <span class='amount'>#{current_order.display_total.to_html}</span>".html_safe
-      css_class = 'full'
+      text = "(#{current_order.item_count})  <span class='amount'>#{current_order.display_total.to_html}</span>".html_safe
+      css_class = 'full cartcolor'
     end
 
     link_to text, spree.cart_path, :class => "cart-info #{css_class}"
